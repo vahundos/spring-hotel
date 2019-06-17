efc43357dbdc15870a446908895c0d2127b7b0d9
Vladimir Avdeev <vahundos@gmail.com>
2019-06-17 14:41:27 +0300

    added project template

---
+++ src/main/java/com/vahundos/spring/hotel/SpringHotelApplication.java
@@ -0,0 +1,13 @@
+package com.vahundos.spring.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHotelApplication.class, args);
+	}
+
+}
