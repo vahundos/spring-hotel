1d44106a1f9511c02cf0ee66b1546cd27ce716f5
Vladimir Avdeev <vahundos@gmail.com>
2019-07-14 16:31:07 +0300

    Task 2 - fixed path to test sql script

--- src/test/java/com/vahundos/spring/hotel/web/controller/NestedSql.java
+++ src/test/java/com/vahundos/spring/hotel/web/controller/NestedSql.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(value = "classpath:hsqldb/data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(value = "classpath:data-hsqldb.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @TestPropertySource(locations = "classpath:test.properties")
 // https://github.com/spring-projects/spring-framework/issues/19930
 @interface NestedSql {
